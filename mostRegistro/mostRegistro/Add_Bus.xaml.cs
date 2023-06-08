8828aed30653fa4b44d37a685e0e04fe02f18e94
Andevc <escobarhandres@gmail.com>
2023-06-07 22:09:58 -0400

    Registro Bus

---
+++ mostRegistro/mostRegistro/Add_Bus.xaml.cs
@@ -0,0 +1,41 @@
+﻿using PumaKatariConsola;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+namespace mostRegistro
+{
+    /// <summary>
+    /// Lógica de interacción para Add_Bus.xaml
+    /// </summary>
+    public partial class Add_Bus : Window
+    {
+        public Add_Bus()
+        {
+            InitializeComponent();
+        }
+        private void btnAddBus(object sender, RoutedEventArgs e)
+        {
+            string placa = txtPlaca.Text;
+            string idBus = txtIdBus.Text;
+            string idConductor = txtIdConductor.Text;
+            string idApoyo = txtIdApoyo.Text;
+            string nomRuta = txtNomRuta.Text;
+            string nroPasajeros = txtNroPasajeros.Text;
+            ArchBus regBus = new ArchBus("RegBuses.dat");
+            regBus.AdiBus(placa,idBus,idConductor,idApoyo,nomRuta,nroPasajeros);
+            this.Close();
+        }
+
+    }
+}
---
+++ mostRegistro/mostRegistro/ArchBus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO ;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace PumaKatariConsola {
+   public class ArchBus {
+      private string nomArch; 
+      public ArchBus(string nomArch) { this.nomArch = nomArch; }
+      public void CrearRegBus(){ if (File.Exists(nomArch)) { File.Delete(nomArch); } } 
+      public void AdiBus(string placa,string idBus, string idCond, string idApoyo, string nomRuta, string nroP){
+         
+         Stream file = File.Open(nomArch, FileMode.OpenOrCreate);
+         BinaryWriter write  = new BinaryWriter(file);
+         try {
+            Bus regBus = new Bus(placa,idBus,nomRuta,idCond,idApoyo,int.Parse(nroP)); 
+            write.Seek(0,SeekOrigin.End);
+            regBus.WrBus(write);
+         }
+         catch (Exception){  }
+         finally { file.Close(); }
+      }
+   }
+}
+
